631ad1c7803b18ce73916b1430cac253caa4a1c5
neimarc <neimarpersonal@gmail.com>
2022-10-24 22:48:57 -0300

    Modificação do import do css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import SignIn from './pages/signin/index';
+import './styles/index.css';
+import { BrowserRouter } from 'react-router-dom';
+import MainRoutes from './routes.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignIn />
+    <BrowserRouter> 
+    {/* Para ficar por volta de todas as rotas criadas no routes.js */}
+    <MainRoutes />
+    {/* Componente onde está todas as rotas */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
