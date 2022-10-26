766c7ff7e519a2e85a0cd15943b0c279e93b4e0a
neimarc <neimarpersonal@gmail.com>
2022-10-25 21:20:18 -0300

    Outras alterações

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import SignIn from "./pages/signin";
+import SignIn from "./pages/signin/index-signin";
 
 function MainRoutes(){
     return(
 
         <Routes>
             <Route path='/' element={<SignIn/>}/>
-        {/* A / significa que é a rota raiz da página */}
+        
         </Routes>
     )
 }
