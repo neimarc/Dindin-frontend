194e02ecd0ff0c051ea60f26f4c561a59852b7ef
neimarc <neimarpersonal@gmail.com>
2022-10-26 23:45:20 -0300

    Adição das rotas SignUp e Main

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/signup/index-signup";
 import SignIn from "./pages/signin/index-signin";
+import Main from "./pages/main/main-index";
 
 function MainRoutes(){
     return(
 
         <Routes>
             <Route path='/' element={<SignIn/>}/>
-        
+        {/* A / significa que é a rota raiz da página */}
+            <Route path='/signup' element={<SignUp/>}/>
+            <Route path='/main' element={<Main/>}/>
         </Routes>
     )
 }
