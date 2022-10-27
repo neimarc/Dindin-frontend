de58a675b5032fc6b4b86c31958d1e53ae65f23c
neimarc <neimarpersonal@gmail.com>
2022-10-26 23:52:21 -0300

    Inicio de produção da página main.

---
+++ src/pages/main/main-index.js
@@ -0,0 +1,12 @@
+import './main-styles.css';
+import Header from '../../components/header/header-index';
+
+function Main() {
+    return (
+        <div className='container-main'>
+            <Header/>
+        </div>
+    )
+}
+
+export default Main;
