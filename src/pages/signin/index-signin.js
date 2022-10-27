824b0c5dba3fa1cde2a102ddb20fbf6c020d2c6f
neimarc <neimarpersonal@gmail.com>
2022-10-26 23:55:46 -0300

    Finalização da página signIn.

--- src/pages/signin/index-signin.js
+++ src/pages/signin/index-signin.js
@@ -1,8 +1,19 @@
 import './signin-styless.css';
 import Logo from '../../assets/logo.svg'
-
+import {useNavigate} from 'react-router-dom';
 
 function SignIn() {
+
+  const navigate = useNavigate();
+  // Para usar a função de navegação para as páginas
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    navigate('/main');
+  }
+  // Para um evento(poderia ser qualquer nome) não forçar uma renderização toda vez.
+
   return ( 
     <div className = 'container-signIn' >
         <img src = {Logo} className = 'logo'alt = 'logo' />
@@ -16,11 +27,13 @@ function SignIn() {
             você tem tudo num único lugar e em um clique de distância. 
             </h3>
 
-            <button className= 'btn-big btn-purple'> Cadastre - se </button> 
+            <button className= 'btn-big btn-purple'
+              onClick={() => navigate('/signup')}> Cadastre - se </button> 
+              {/* Chamada do navigate para ir para a página signup definida no routes */}
           </div>
 
           <div className = 'right' >
-            <form >
+            <form onSubmit={handleSubmit} >
             <h2> Login </h2> 
               <div className='inputs-container'>
                 <label htmlFor = 'email' > E-mail </label>
