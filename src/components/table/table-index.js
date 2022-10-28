69eb6cbd2d51bfcd663593f08f624b078b5a2476
neimarc <neimarpersonal@gmail.com>
2022-10-27 23:57:08 -0300

    Adição do componente table

---
+++ src/components/table/table-index.js
@@ -0,0 +1,24 @@
+import './table-styles.css';
+
+function Table() {
+    return (
+        <div className='container-table'>
+
+            <div className='table-head'>
+                <div className='table-column'>
+
+                </div>
+            </div>
+
+            <div className='table-body'>
+                <div className='table-line'>
+
+                </div>
+            </div>
+
+            Teste de tabela
+        </div>
+    )
+}
+
+export default Table;
