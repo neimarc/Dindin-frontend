66d4808b08316cc842bdd0dfd0251718d640974d
neimarc <neimarpersonal@gmail.com>
2022-10-27 23:31:43 -0300

    Implementação de funcionalidade do logout

--- src/components/header/header-index.js
+++ src/components/header/header-index.js
@@ -2,18 +2,27 @@ import './header-styles.css';
 import Logo from '../../assets/logo.svg'
 import Profile from '../../assets/profile.svg'
 import Logout from '../../assets/logout.svg'
+import { useNavigate } from 'react-router-dom';
 
 
 function Header() {
+    const navigate = useNavigate();
+
+    function handleLogout(){
+        navigate('/'); 
+    }
     return (
         <header>
-            <img src={Logo} alt='logo'/>
-            <div>
-                <div>
-                    <img src={Profile} alt='profile'/>
-                    <strong>Neimar</strong>
+            <div className='content-header width-max'> {/* Para limitar o tamanho máximo do header  */}
+                <img src={Logo} alt='logo'/>
+                <div className='container-signOut'>
+                    <div className='profile-place'>
+                        <img src={Profile} alt='profile'/>
+                        <strong>Neimar</strong>
+                    </div>
+                    <img className='signOut' src={Logout} alt='logout'
+                    onClick={handleLogout}/>
                 </div>
-                <img className='signOut' src={Logout} alt='logout'/>
             </div>
         </header>
     )
