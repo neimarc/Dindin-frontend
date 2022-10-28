9de611caab405e58477006d65c29a0e0dd534bd1
neimarc <neimarpersonal@gmail.com>
2022-10-27 23:58:11 -0300

    Adição da tabela

--- src/pages/main/main-index.js
+++ src/pages/main/main-index.js
@@ -1,10 +1,15 @@
 import './main-styles.css';
 import Header from '../../components/header/header-index';
+import Table from '../../components/table/table-index';
 
 function Main() {
     return (
         <div className='container-main'>
             <Header/>
+
+            <section>
+                <Table/>
+            </section>
         </div>
     )
 }
