f6311a09c6b7ddf1fadc403d8063187c73bcf865
neimarc <neimarpersonal@gmail.com>
2022-10-29 23:20:19 -0300

    Adição da sessão da tabela de resumo

--- src/pages/main/main-index.js
+++ src/pages/main/main-index.js
@@ -1,6 +1,7 @@
 import './main-styles.css';
 import Header from '../../components/header/header-index';
 import Table from '../../components/table/table-index';
+import ResumeTable from '../../components/resume-table/resume-index';
 
 function Main() {
     return (
@@ -8,7 +9,13 @@ function Main() {
             <Header/>
 
             <section>
-                <Table/>
+                <div className='width-limit'> {/*Para limitar o tamanho da sessão com a tabela */}
+                    <button>Filtros</button>
+                    <div className='container-data'>
+                        <Table/>
+                        <ResumeTable/>
+                    </div>
+                </div>
             </section>
         </div>
     )
