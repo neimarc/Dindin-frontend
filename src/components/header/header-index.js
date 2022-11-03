cecc00396be0f2d7dbbced721cf9c80aab0dc412
neimarc <neimarpersonal@gmail.com>
2022-11-02 23:58:21 -0300

    Adição de evento

--- src/components/header/header-index.js
+++ src/components/header/header-index.js
@@ -4,19 +4,21 @@ import Profile from '../../assets/profile.svg'
 import Logout from '../../assets/logout.svg'
 import { useNavigate } from 'react-router-dom';
 
-
-function Header() {
+// O header recebe a prop editProfile do
+function Header(editProfile) {
     const navigate = useNavigate();
 
     function handleLogout(){
-        navigate('/'); 
+        navigate('/'); {/*Para ir para a página raiz */}
     }
     return (
         <header>
             <div className='content-header width-max'> {/* Para limitar o tamanho máximo do header  */}
                 <img src={Logo} alt='logo'/>
                 <div className='container-signOut'>
-                    <div className='profile-place'>
+                    <div 
+                    className='profile-place'
+                    onClick={editProfile}>
                         <img src={Profile} alt='profile'/>
                         <strong>Neimar</strong>
                     </div>
