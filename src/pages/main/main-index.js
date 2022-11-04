9d4c2980b0fad6e7152682924c35923998b0472d
neimarc <neimarpersonal@gmail.com>
2022-11-03 23:58:35 -0300

    Adição de estado

--- src/pages/main/main-index.js
+++ src/pages/main/main-index.js
@@ -2,11 +2,17 @@ import './main-styles.css';
 import Header from '../../components/header/header-index';
 import Table from '../../components/table/table-index';
 import ResumeTable from '../../components/resume-table/resume-index';
+import ProfileModal from '../../components/profile-modal/profile-index';
+import { useState } from 'react';
 
 function Main() {
+
+    const [openProfile, setOpenProfile] = useState(false); //Para abrir o modal do profile
+
     return (
         <div className='container-main'>
-            <Header/>
+        {/* No Header está o ícone do profile que ativa o modal e a declaraçao da prop editProfile */}
+            <Header editProfile={() => setOpenProfile(true)}/>
 
             <section>
                 <div className='width-limit'> {/*Para limitar o tamanho da sessão com a tabela */}
@@ -21,6 +27,9 @@ function Main() {
                     </div>
                 </div>
             </section>
+            <ProfileModal
+            open={openProfile}
+            close={() => setOpenProfile(false)}/> {/*Para fechar o modal profile */}
         </div>
     )
 }
