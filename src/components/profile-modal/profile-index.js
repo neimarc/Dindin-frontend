0ff1d108fc6ff228d77dc3e97a009d457e8dd50b
neimarc <neimarpersonal@gmail.com>
2022-11-03 23:57:16 -0300

    Adicionais

--- src/components/profile-modal/profile-index.js
+++ src/components/profile-modal/profile-index.js
@@ -1,20 +1,37 @@
 import './profile.css';
 import CloseIcon from '../../assets/close-icon.svg'
 
-function ProfileModal({open, close}) {
+function ProfileModal(open, close) {
     return (
         <>
             {open &&
                 <div className='outside-back'>
-                    <img src={CloseIcon} alt='close-button'/>
-                    <h2>Editar Perfil</h2>
+                    <div className='modal'>
 
-                    <form>
-                        <div className='inputs-container'>
-                            <label>Nome</label>
-                            <input type='text'/>
-                        </div>
-                    </form>
+                        <img className='button-close' src={CloseIcon} alt='close-button'/>
+                        <h2>Editar Perfil</h2>
+                        <form>
+                            <div className='inputs-container'>
+                                <label>Nome</label>
+                                <input type='text'/>
+                            </div>
+                            <div className='inputs-container'>
+                                <label>E-mail</label>
+                                <input type='text'/>
+                            </div>
+                            <div className='inputs-container'>
+                                <label>Senha</label>
+                                <input type='password'/>
+                            </div>
+                            <div className='inputs-container'>
+                                <label>Confirmação de senha</label>
+                                <input type='password'/>
+                            </div>
+                            <button className='btn-purple btn-small' >Confirmar</button>
+                        </form>
+
+                    </div>
+
                 </div>
             }
         </>
