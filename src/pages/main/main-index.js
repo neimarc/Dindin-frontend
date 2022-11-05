167584318886672c0b2da33cb9b1d42f688f1a6b
neimarc <neimarpersonal@gmail.com>
2022-11-04 23:55:12 -0300

    Adição da lógica do modal de transação

--- src/pages/main/main-index.js
+++ src/pages/main/main-index.js
@@ -4,14 +4,16 @@ import Table from '../../components/table/table-index';
 import ResumeTable from '../../components/resume-table/resume-index';
 import ProfileModal from '../../components/profile-modal/profile-index';
 import { useState } from 'react';
+import TransactionModal from '../../components/transaction-modal/transaction-index';
 
 function Main() {
 
     const [openProfile, setOpenProfile] = useState(false); //Para abrir o modal do profile
+    const [openTransactionModal, setOpenTransactionModal] = useState(false);
 
     return (
         <div className='container-main'>
-        {/* No Header está o ícone do profile que ativa o modal e a declaraçao da prop editProfile */}
+        {/* No Header está o ícone do profile e a declaraçao da prop editProfile que ativa o modal  */}
             <Header editProfile={() => setOpenProfile(true)}/>
 
             <section>
@@ -22,14 +24,23 @@ function Main() {
 
                         <div className='right-side'> {/*O lado direito da página*/}
                         <ResumeTable/>
-                        <button className='btn-purple btn-small'>Adicionar Registro</button>
+                        <button className='btn-purple btn-small'
+                        onClick={() => setOpenTransactionModal(true)}>
+                            Adicionar Registro</button>
                         </div>
                     </div>
                 </div>
             </section>
+
             <ProfileModal
             open={openProfile}
             close={() => setOpenProfile(false)}/> {/*Para fechar o modal profile */}
+            <TransactionModal
+            open={openTransactionModal} 
+            close={() => setOpenTransactionModal(false)} //Lógica de fechar o modal de adicionar transação
+            // As propos do modal de transação são declaradas em transaction-index.js
+            />
+
         </div>
     )
 }
