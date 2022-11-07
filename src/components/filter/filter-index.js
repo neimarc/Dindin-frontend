2b17f6358fc0c96f83e832bef7d3dd799987c55a
neimarc <neimarpersonal@gmail.com>
2022-11-06 23:53:28 -0300

    Adição de classes e outros botões

--- src/components/filter/filter-index.js
+++ src/components/filter/filter-index.js
@@ -13,19 +13,22 @@ function Filter() {
         <div className='filter-box'>
             {/* Se o open está setado como false, quando clicar o setOpen se torna true (open negado) e mostra o modal */}
             <button onClick={() => setOpen(!open)}
-                className='btn-filter'>
+                className='btn-filter btn-white'>
                 <img src={FilterIcon} alt='filter' /> Filtrar</button>
 
             {open &&
                 <div className='filter-body'>
                     <strong>Categoria</strong>
-                    <div>
+
+                    <div className='categories-content'>
                         <Tag title='Compras' checked />
                         <Tag title='Vendas' checked={false} />
-
-
                     </div>
 
+                    <div className='buttons-filter'>
+                        <button className='btn-white btn-extra-small'>Limpar Filtros</button>
+                        <button className='btn-purple btn-extra-small'>Aplicar Filtros</button>
+                    </div>
                 </div>
             }
         </div>
