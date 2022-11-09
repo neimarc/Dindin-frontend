81f36ccbc261248732d5458f74026d564bfdbf93
neimarc <neimarpersonal@gmail.com>
2022-11-08 23:59:19 -0300

    Configuração do axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+//Configuração básico de axios que serve para fazer a integração com o backend
 export default axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://localhost:3000', //Local ende está a api do backend
     timeout: 10000,
     headers: {
-        'Content-Type': 'aplication/json'
+        'Content-Type': 'aplication/json' //Para sinalizar que os dados que serão transitados estão em formato json
     }
 })
