c3f72ecb9defa45290dd371845ec190eebede1fc
neimarc <neimarpersonal@gmail.com>
2022-11-08 23:59:47 -0300

    Configuração do localStorage

---
+++ src/utils/storage.js
@@ -0,0 +1,19 @@
+//Para leitura e escrita dentro do local storage
+
+export function setItem(key, value) {
+    localStorage.setItem(key, value)
+}
+
+export function getItem(key) {
+    return localStorage.getItem(key)
+}
+
+export function clear() {
+    localStorage.clear()
+}
+
+export function removeItem(key) {
+    localStorage.removeItem(key)
+}
+
+//Todos são localStorage.métodos
