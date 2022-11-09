44c62282f227edcbd531d054c871d78dd8282273
neimarc <neimarpersonal@gmail.com>
2022-11-08 23:58:09 -0300

    Adição de rota protegida

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import SignUp from "./pages/signup/index-signup";
 import SignIn from "./pages/signin/index-signin";
 import Main from "./pages/main/main-index";
+import { getItem } from "./utils/storage";
 
-function MainRoutes(){
-    return(
+function ProtectRoutes({ redirectTo }) {
+    const token = getItem('token');
+    return token ? <Outlet /> : <Navigate to={redirectTo} />
+}
+
+function MainRoutes() {
+    return (
 
         <Routes>
-            <Route path='/' element={<SignIn/>}/>
-        {/* A / significa que é a rota raiz da página */}
-            <Route path='/signup' element={<SignUp/>}/>
-            <Route path='/main' element={<Main/>}/>
-        </Routes>
+            <Route path='/' element={<SignIn />} />
+            {/* A / significa que é a rota raiz da página */}
+            <Route path='/signup' element={<SignUp />} />
+            <Route element={<ProtectRoutes redirectTo='/' />}>
+                <Route path='/main' element={<Main />} />
+            </Route>
+        </Routes >
     )
 }
 
