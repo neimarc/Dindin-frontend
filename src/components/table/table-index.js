1ddd5358072e074d41e016bef06d166d49ac859f
neimarc <neimarpersonal@gmail.com>
2022-11-09 23:55:11 -0300

    Adição das propriedades de transação

--- src/components/table/table-index.js
+++ src/components/table/table-index.js
@@ -6,50 +6,56 @@ import ArrowDown from '../../assets/arrow-down.svg';
 import { useState } from 'react';
 import ConfirmModal from '../confirm-modal/confirm-index';
 
-
-function Table() {
+// Transactions vem da Main
+function Table({ transactions }) {
     const [arrowUp, setArrowUp] = useState(true); //Para mudar a seta para baixo ou para cima
     const [openModal, setOpenModal] = useState(false); //Para esconder (false) ou mostrar o modal (true)
-    
+
     function occultModal() {
         console.log('delete');
         setOpenModal(false);
-;    }
+        ;
+    }
     return (
         <div className='container-table'>
 
             <div className='table-head'>
-               <div className='table-column-small date-column'
-               onClick={() => setArrowUp(!arrowUp)}>
-                <strong >Data</strong>
-                <img src={arrowUp ? ArrowUp : ArrowDown} alt='arrow-up'></img>
-               </div>
+                <div className='table-column-small date-column'
+                    onClick={() => setArrowUp(!arrowUp)}>
+                    <strong >Data</strong>
+                    <img src={arrowUp ? ArrowUp : ArrowDown} alt='arrow-up'></img>
+                </div>
                 <strong className='table-column-middle'>Dia da semana</strong>
                 <strong className='table-column-big'>Descrição</strong>
                 <strong className='table-column-small'>Categoria</strong>
                 <strong className='table-column-small'>Valor</strong>
                 <div className='table-column-small'></div>
-              
+
             </div>
 
-            <div className='table-body'>
-                <div className='table-line'>
-                    <strong className='table-column-small date-column'>11/04/2022</strong>
-                    <span className='table-column-middle'>Segunda</span>
-                    <span className='table-column-big'>Venda de um Palio</span>
-                    <span className='table-column-small'>Vendas</span>
-                    <strong className='table-column-small'>R$ 12.000,00</strong>
-                    <div className='table-column-small hand-buttons'>
-                        <img src={EditIcon} alt='edit' />
-                        <img src={DeleteIcon} alt='delete' 
-                        onClick={() => setOpenModal(true)}/> {/*Para quando clicar no ícone o modal aparecer*/}
+            {/* As propriedades id, data, descricao, categoria_nome e valor, abaixo, foram tiradas da documentação da api */}
 
+            <div className='table-body'>
+                {transactions.map((trans) => (
+                    <div className='table-line' key={trans.id}>
+                        <strong className='table-column-small date-column'>{trans.data}</strong>
+                        <span className='table-column-middle'>{trans.data}</span>
+                        <span className='table-column-big'>{trans.descricao}</span>
+                        <span className='table-column-small'>{trans.categoria_nome}</span>
+                        <strong className='table-column-small'>trans.valor</strong>
+                        <div className='table-column-small hand-buttons'>
+                            <img src={EditIcon} alt='edit' />
+                            <img src={DeleteIcon} alt='delete'
+                                onClick={() => setOpenModal(true)} /> {/*Para quando clicar no ícone o modal aparecer*/}
+
+                        </div>
+                        <ConfirmModal
+                            open={openModal}
+                            close={() => setOpenModal(false)}
+                            confirm={occultModal} />
                     </div>
-                    <ConfirmModal 
-                    open={openModal} 
-                    close={() => setOpenModal(false)}
-                    confirm={occultModal}/>
-                </div>
+                ))}
+
             </div>
 
         </div>
