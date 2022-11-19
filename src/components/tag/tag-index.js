ec924368bcf01ff6562845d245737901a7c5b388
neimarc <neimarpersonal@gmail.com>
2022-11-18 23:58:28 -0300

    Implementação de de dinamicidade das tags

--- src/components/tag/tag-index.js
+++ src/components/tag/tag-index.js
@@ -1,6 +1,20 @@
 import './tag-styles.css';
 
-function Tag({ title, checked }) {
+function Tag({ title, checked, id, categories, setCategories }) {
+
+    function categorieChecked() {
+        const categoriesArea = [...categories];
+
+        categoriesArea.forEach((categ) => {
+            if (categ.id === id) {
+                categ.checked = !categ.checked
+            }
+        })
+
+        setCategories([...categoriesArea])
+
+    }
+
     return (
         <div className={`${checked ? 'checked' : 'unchecked'} tag-box`}>
             <span>{title}</span>
