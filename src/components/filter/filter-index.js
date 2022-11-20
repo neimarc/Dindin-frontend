337258f87d8914a9d4d631726c8fb5a5576f8123
neimarc <neimarpersonal@gmail.com>
2022-11-19 22:45:44 -0300

    Implementação do filtro da tabela

--- src/components/filter/filter-index.js
+++ src/components/filter/filter-index.js
@@ -2,13 +2,54 @@ import { useEffect, useState } from 'react';
 import './filter-styles.css';
 import FilterIcon from '../../assets/filter-icon.svg'
 import Tag from '../tag/tag-index';
-import { fillCategories } from '../../utils/requisitions';
+import { fillCategories, loadTransactions } from '../../utils/requisitions';
 
 
 function Filter({ transactions, setTransactions }) {
 
     const [open, setOpen] = useState(false);
     const [categories, setCategories] = useState([]);
+
+
+    //Para limpar filtros
+    async function clearFilters() {
+        const categoriesArea = [...categories]; //Para pegar as categorias
+        categoriesArea.forEach(categ => categ.checked = false) //Por ser uma expressão simples, não precisa abrir chaves
+        
+        setCategories([...categoriesArea]) //Para receber todas as categorias negadas
+
+        const everyTransaction = await loadTransactions();
+
+        setTransactions([...everyTransaction]) //Para quando clicar em limpar filtros, recarregue todas as transações
+
+    }
+
+    //Para filtrar todas as transações selecionadas de acordo com parâmetro pesquisado
+    async function ApplyFilter() {
+        const transactionArea = await loadTransactions(); //Se não tiver nenhuma categoria selecionada, então traz todas que já estão
+        setTransactions([...transactionArea]) //Atualizando com todas as transações
+
+        const checkedCategories = [];
+
+        categories.forEach((categ) => {
+            if (categ.checked) {
+                checkedCategories.push(categ.id) //Para armazenar todas as informações de id das categorias pesquisadas
+            }
+        })
+
+        //Isso !checkedCategories.length é igual a checkedCategories.length === 0
+        if (!checkedCategories.length) {
+            
+            return;
+        }
+
+        //Verifica se nas transações têm o id das categorias filtradas (coincidência de id). Se sim, armazena na constante filteredTransactions
+        const filteredTransactions = transactionArea.filter((trans) =>
+            checkedCategories.includes(trans.categoria_id))
+
+        //Atualiza o setTransactions com os transações filtradas
+        setTransactions([...filteredTransactions])
+    }
 
     useEffect(() => {
         async function receiveEveryCategorie() {
@@ -57,8 +98,16 @@ function Filter({ transactions, setTransactions }) {
                     </div>
 
                     <div className='buttons-filter'>
-                        <button className='btn-white btn-extra-small'>Limpar Filtros</button>
-                        <button className='btn-purple btn-extra-small'>Aplicar Filtros</button>
+                        <button
+                            className='btn-white btn-extra-small'
+                            onClick={clearFilters}>
+                            Limpar Filtros
+                        </button>
+                        <button
+                            className='btn-purple btn-extra-small'
+                            onClick={ApplyFilter}>
+                            Aplicar Filtros
+                        </button>
                     </div>
                 </div>
             }
